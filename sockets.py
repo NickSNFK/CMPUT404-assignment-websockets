0565fda1fbfd9dc1ca3e63e6b4eab8f76908e0b1
NickSNFK <nwielgus@hotmail.com>
2023-03-30 21:46:32 -0600

    Update sockets.py

--- sockets.py
+++ sockets.py
@@ -88,12 +88,11 @@ def subscribe_socket(ws):
         while not ws.closed:
             message = ws.receive()
             if message:
-                print(message)
                 entity = json.loads(message)
                 for key in entity.keys():
                     myWorld.set(key,entity['key'])
     except Exception as e:
-        print(e)
+        print(e.with_traceback())
     finally:
         myWorld.remove_client(ws)
 
